b80c7b9cbc6802bc6b8957bbf76ed175d5361876
Khachatryan Nikolya Sergeevich <164537426+Nikolya21@users.noreply.github.com>
2026-07-09 16:41:12 +0300

    find alone number among values that have three repeats

---
+++ Array/AloneNumber.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int one = 0;
+        int two = 0;
+        for (int num : nums) {
+            one ^= (num & ~two);
+            two ^= (num & ~one);
+        }
+        return one;
+    }
+}
